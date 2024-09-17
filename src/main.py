ac26853b38833aaebf7ceed0a6e1c087744e3e22
Trancos91 <122824086+Trancos91@users.noreply.github.com>
2024-09-17 03:30:07 -0300

    Más mensajes de debug

--- src/main.py
+++ src/main.py
@@ -18,7 +18,9 @@ def handle_message(texto: str, update: Update):
     #if error := chequear_usuarix(update):
     #    return error
 
+    print("Corriendo handle_message")
     respuesta = Respuestas(texto, update).respuestas()
+    print("Corrido 'Respuestas.respuestas(), devolviendo respuesta...")
     return respuesta
 
 @requiere_usuarix
